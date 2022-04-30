675c795ba11eb858f4d9b8b4bf06924ab6cf43ca
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2022-04-30 13:38:28 +0800

    add test workflow.

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ros_distro=$1
 ros_workspace=$2
+hastimestamp=$3
 tmp_space="/tmp/catkin_binary_deb"
 
 source /opt/ros/$ros_distro/setup.bash
@@ -12,7 +13,7 @@ time_stamp=`date +%Y%m%d.%H%M%S`
 
 mkdir $tmp_space
 cp -r ./ $tmp_space
-package_list=`find $tmp_space -maxdepth 2 -name package.xml | sed 's/package.xml//g'`
+package_list=`find $tmp_space -name package.xml | sed 's/package.xml//g'`
 CM_PREFIX_PATH=`sed 's/:/;/g' <<< $CMAKE_PREFIX_PATH`
 
 for package_source in $package_list
@@ -20,7 +21,7 @@ do
   echo "Trying to package $package_source..."
   cd $package_source
   bloom-generate rosdebian --os-name ubuntu --ros-distro $ros_distro
-  if [ ${{ inputs.timestamp }} ]
+  if [ $hastimestamp ]
   then
     debchange -a $time_stamp -p -D -u -m 'Append timestamp when binarydeb was built.'
   fi
